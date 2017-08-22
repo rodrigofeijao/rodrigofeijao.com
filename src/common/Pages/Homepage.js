5d8b773af84f0f94a71fc279c3cfe24010cff431
Rodrigo Feijao <rodrigo.feijao@saatchi.co.uk>
2017-08-22 09:59:29 +0100

    GSAP added to project. Some tests implemented.

--- src/common/Pages/Homepage.js
+++ src/common/Pages/Homepage.js
@@ -3,12 +3,35 @@
  */
 import React from 'react';
 import Helmet from 'react-helmet';
+import {TweenMax, TimelineLite} from 'gsap';
 
 
 export default class Homepage extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.startAnim = this.startAnim.bind(this);
+    }
+
+    componentDidMount() {
+        var tl = new TimelineLite({ paused : true });
+
+        tl
+        .to(this.block, 0.45, {width : '100%'})
+        .to(this.blockIntro, 0.45, {width : '100%'}, '-=.20')
+        .to(this.greeting, 0.05, {color : 'rgba(#000, 1)'})
+        .to(this.intro, 0.05, {color : 'rgba(#000, 1)'}, '-=.10')
+        .to(this.block, 0.35, {left : '100%'})
+        .to(this.blockIntro, 0.35, {left : '100%'}, '-=.10')
+
+        // Start delayed animation
+        setTimeout( ()=>{ tl.play() } , 1200);
+
+    }
+
+    startAnim() {
+        
     }
 
     render() {
@@ -22,13 +45,15 @@ export default class Homepage extends React.Component {
                 ]} />
 
                 <div className="shadow"></div>
-
-                <div className="greeting">
+                
+                <div className="greeting" ref={(input) => { this.greeting = input; }} >
+                    <span className="block" ref={(input) => { this.block = input; }}></span>
                     hello there.<br/>
                     My name is Rodrigo I’m a
                 </div>
-
-                <div className="intro">Creative Technologist<br/>
+                <div className="intro" ref={(input) => { this.intro = input; }}>
+                    <span className="block" ref={(input) => { this.blockIntro = input; }}></span>
+                    Creative Technologist<br/>
                     and a problem solver,<br/>
                     based in central London.
                 </div>
